2b82338b97efc272271842436fa24086b8ebc240
CamilleKhv <camille.kerherve@gmail.com>
2024-01-26 17:56:34 +0100

    deuxieme commit

--- src/main/resources/Supprimer.js
+++
@@ -1,8 +0,0 @@
-let cible = document.getElementById("supprimer") ;
-cible.addEventListener("click",action) ;
-function action(event){
-    let ligneASuppr = element.parentNode.parentNode ;
-    alert("clic sur le button : " + event.cible.id);
-    ligneASuppr.remove() ;
-}
-
